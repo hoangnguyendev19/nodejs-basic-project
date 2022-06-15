d75c739ac2c1549c70fe38d0c373a479f1befc74
hoangnguyendev <hoangngocnguyen2021@gmail.com>
2022-06-15 17:25:43 +0700

    update the first

---
+++ .history/src/server_20220615171838.js
@@ -0,0 +1,19 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+
+const app = express();
+const port = 8080;
+
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
