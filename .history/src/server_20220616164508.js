24e92d037c70ed587fc80a2b989c4ceab1772d6b
hoangnguyendev <hoangngocnguyen2021@gmail.com>
2022-06-16 17:15:24 +0700

    update the second

--- .history/src/server_20220616164508.js
+++ .history/src/server_20220616164508.js
@@ -1,14 +1,11 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
-import initWebRoute from "./route/web";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 configViewEngine(app);
-
-initWebRoute(app);
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
